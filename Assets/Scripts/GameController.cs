7bcf9ff9a28b820f73372cf541a30a2e34cf82ce
Blackhawk <daniel.peters1@pm.me>
2020-11-20 17:57:34 +0100

    Revert getter setter commit

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -33,9 +33,9 @@ public class GameController : MonoBehaviour
 	private RectTransform redHealthBarRect;
 
 	//Damage Timer
-	private bool startTimerGameOverExplosion = false;
-	private float timerGameOverExplosion = 2;
-	private int timerGameOverExplosionInt;
+	private bool startTimerGameOverExpolion = false;
+	private float timerGameOverExpolion = 2;
+	private int timerGameOverExpolionInt;
 
 	//Level
 	public GameObject level1;
@@ -74,17 +74,41 @@ public class GameController : MonoBehaviour
 	public RectTransform GreenHealthBarRect => greenHealthBarRect;
 	public RectTransform RedHealthBarRect => redHealthBarRect;
 
-	public bool StartTimerRedHealth { get; set; }
+	public bool StartTimerstartTimerRedHealth
+	{
+		get => startTimerRedHealth;
+		set => startTimerRedHealth = value;
+	}
 
-	public float TimerRedHealth { get; set; }
+	public float TimerRedHealth
+	{
+		get => timerRedHealth;
+		set => timerRedHealth = value;
+	}
 
-	public int TimerRedHealthInt { get; set; }
+	public int TimerRedHealthInt
+	{
+		get => timerRedHealthInt;
+		set => timerRedHealthInt = value;
+	}
 
-	public bool StartTimerGameOverExplosion { get; set; }
+	public bool StartTimerGameOverExpolion
+	{
+		get => startTimerGameOverExpolion;
+		set => startTimerGameOverExpolion = value;
+	}
 
-	public float TimerGameOverExplosion { get; set; }
+	public float TimerGameOverExpolion
+	{
+		get => timerGameOverExpolion;
+		set => timerGameOverExpolion = value;
+	}
 
-	public int TimerGameOverExplosionInt { get; set; }
+	public int TimerGameOverExpolionInt
+	{
+		get => timerGameOverExpolionInt;
+		set => timerGameOverExpolionInt = value;
+	}
 
 
 	private bool startTimerWinText = false;
