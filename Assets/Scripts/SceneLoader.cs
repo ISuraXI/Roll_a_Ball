8006f26aa1dfd83b7186f0a420c8a63c067048d8
Tobias Jaeger <45832814+ISuraXI@users.noreply.github.com>
2020-11-22 22:38:31 +0100

    add Menu screen with level menu

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+	public void RollaBallScene()
+	{
+		SceneManager.LoadScene("RollABall");
+	}
+
+	public void menuScene()
+	{
+		SceneManager.LoadScene("Menu");
+	}
+}
