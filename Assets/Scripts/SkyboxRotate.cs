8876d95e6471ae643aade48ff9cf8ddf91caca1c
Tobias Jaeger <45832814+ISuraXI@users.noreply.github.com>
2020-11-23 18:49:13 +0100

    add in menu skybox rotation

---
+++ Assets/Scripts/SkyboxRotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxRotate : MonoBehaviour
+{
+	public float Skyboxspeed;
+
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		RenderSettings.skybox.SetFloat("_Rotation", Time.time * Skyboxspeed);
+	}
+}
